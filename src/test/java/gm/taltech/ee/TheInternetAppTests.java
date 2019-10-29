ebf3091b1b2f88eecca9df02f7613d8e590074c3
German Mumma <german.mumma@ttu.ee>
2019-10-29 17:20:11 +0200

    add dynamic loading (web drive wait example)

--- src/test/java/gm/taltech/ee/TheInternetAppTests.java
+++ src/test/java/gm/taltech/ee/TheInternetAppTests.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -16,6 +18,8 @@ import org.testng.annotations.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.openqa.selenium.By.cssSelector;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class TheInternetAppTests {
 
@@ -50,13 +54,25 @@ public class TheInternetAppTests {
         // click Login button
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         // should see success notification
-        WebElement webElement = driver.findElement(By.cssSelector(".flash.success"));
+        WebElement webElement = driver.findElement(cssSelector(".flash.success"));
         assertThat(webElement, notNullValue());
     }
 
     @Test
-    public void should_see_hello_world_after_loading(){
+    public void should_see_hello_world_after_loading() throws InterruptedException {
+        // got to Dynamic Loading page
+        driver.findElement(By.linkText("Dynamic Loading")).click();
+        // Click Example 1 link
+        driver.findElement(By.linkText("Example 1: Element on page that is hidden")).click();
+        // Click button Start
+        driver.findElement(By.cssSelector("#start > button")).click();
+        // should see Hello World
 
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10000);
+        WebElement helloWorldElement = webDriverWait.until(visibilityOfElementLocated(cssSelector("#finish > h4")));
+
+        String helloWorld = helloWorldElement.getText();
+        assertThat(helloWorld, is("Hello World!"));
     }
 
     @AfterClass
