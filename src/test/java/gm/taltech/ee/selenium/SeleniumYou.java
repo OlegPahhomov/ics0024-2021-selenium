8cc69244e751110289af481042ebba6e0226563a
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2021-10-17 11:55:01 +0300

    compiling project

--- src/test/java/gm/taltech/ee/selenium/SeleniumYou.java
+++ src/test/java/gm/taltech/ee/selenium/SeleniumYou.java
@@ -1,31 +1,31 @@
-package gm.taltech.ee.selenium;
-
-import gm.taltech.ee.selenium.common.SeleniumTest;
-import org.testng.annotations.Test;
-
-public class SeleniumYou extends SeleniumTest {
-
-    @Test
-    public void should_not_login_with_invalid_login() {
-    }
-
-    @Test
-    public void should_not_login_with_invalid_password() {
-    }
-
-    @Test
-    public void should_login_then_logout() {
-    }
-
-    @Test
-    public void should_change_location_of_blocks_after_drag_and_drop_to_BA() {
-    }
-
-    @Test
-    public void should_not_change_location_of_blocks_after_drag_and_drop_to_not_B() {
-    }
-
-    @Test
-    public void should_see_text_new_window_after_clicking_the_link_click_here() {
-    }
-}
+//package gm.taltech.ee.selenium;
+//
+//import gm.taltech.ee.selenium.common.SeleniumTest;
+//import org.testng.annotations.Test;
+//
+//public class SeleniumYou extends SeleniumTest {
+//
+//    @Test
+//    public void should_not_login_with_invalid_login() {
+//    }
+//
+//    @Test
+//    public void should_not_login_with_invalid_password() {
+//    }
+//
+//    @Test
+//    public void should_login_then_logout() {
+//    }
+//
+//    @Test
+//    public void should_change_location_of_blocks_after_drag_and_drop_to_BA() {
+//    }
+//
+//    @Test
+//    public void should_not_change_location_of_blocks_after_drag_and_drop_to_not_B() {
+//    }
+//
+//    @Test
+//    public void should_see_text_new_window_after_clicking_the_link_click_here() {
+//    }
+//}
